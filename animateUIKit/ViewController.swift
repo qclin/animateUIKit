8859f09c3ed5e9fc69b58bd4696225f2d00836bc
Qiao Lin <qiaoclin@gmail.com>
2017-03-02 13:57:47 +0100

    load live view to playground

--- animateUIKit/ViewController.swift
+++ animateUIKit/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let colors: [UIColor] = [
+        .red,
+        .blue,
+        .yellow,
+        .orange,
+        .green,
+        .brown
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var animatingView: UIView!
 
+    @IBAction func animatingViewTapped(_ sender: Any) {
+        let animator = UIViewPropertyAnimator(duration: 1.0, curve: .easeIn){
+            [weak animatingView, weak self] in
+            
+            guard
+                let view = animatingView,
+                let strongSelf = self,
+                let viewBackgroundColor = view.backgroundColor
+                else {return}
+            
+            view.backgroundColor = strongSelf.randomColor(noEqualTo: viewBackgroundColor)
+        }
+        
+        animator.startAnimation()
+    }
+    
+    
+    func randomColor(noEqualTo currentColor: UIColor) -> UIColor{
+        var foundColor = currentColor
+        
+        repeat{
+            let index = Int(arc4random_uniform(UInt32(colors.count)))
+            foundColor = colors[index]
+        } while foundColor.isEqual(currentColor)
+        
+        return foundColor
+    }
+    
 }
 
--- liveViews.playground/Contents.swift
+++ liveViews.playground/Contents.swift
@@ -3,3 +3,24 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+import UIKit
+import PlaygroundSupport
+
+extension Double{
+    var toSize: CGSize{
+        return .init(width: self, height: self)
+    }
+}
+
+extension CGSize{
+    var toRectWithZeroOrigin: CGRect{
+        return CGRect(origin: .zero, size: self)
+    }
+}
+
+let view = UIView(frame: 300.toSize.toRectWithZeroOrigin)
+view.backgroundColor = .blue
+PlaygroundPage.current.liveView = view
+
